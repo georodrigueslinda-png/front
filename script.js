c27eff3491f6549cf8eee07eb77c3c833ddfeb34
unasptrabalho <geovannarodriguesp@icloud.com>
2025-10-20 20:17:28 -0300

    okk

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const button = document.querySelector("button")
-
+const main = document.querySelector("main")
 button.addEventListener("click", buscarUsuarios)
 
-function buscarUsuarios() {
-    fetch("http://localhost:3333").then((response) => response.json()).then((usuarios) => console.log(usuarios))
+async function buscarUsuarios() {
+const users = await fetch("http://localhost:3333").then((response) => response.json())
+
+users.map(user => {
+ main.innerHTML += `
+  <section>
+            <h3>Nome: ${user.name}</h3>
+            <p>Idade: ${user.age}</p>
+            <p>E-mail: ${user.email}</p>
+        </section>
+        `
+})
 }
