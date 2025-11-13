73f7e0379525c6693039fe31997e756284cb88e1
unasptrabalho <geovannarodriguesp@icloud.com>
2025-11-13 18:40:32 -0300

    ok

--- pages/usuarios.js
+++ pages/usuarios.js
@@ -1,6 +1,5 @@
-const button = document.querySelector("button")
 const main = document.querySelector("main")
-button.addEventListener("click", buscarUsuarios)
+
 
 async function buscarUsuarios() {
 const users = await fetch("http://localhost:3333").then((response) => response.json())
@@ -8,10 +7,12 @@ const users = await fetch("http://localhost:3333").then((response) => response.j
 users.map(user => {
  main.innerHTML += `
   <section>
-            <h3>Nome: ${user.name}</h3>
+            <h2>Nome: ${user.name}</h2>
             <p>Idade: ${user.age}</p>
             <p>E-mail: ${user.email}</p>
         </section>
         `
 })
 }
+
+buscarUsuarios()
